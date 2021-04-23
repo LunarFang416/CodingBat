7233d9ab9e2d3643406aaa1f293f0d92cbbabed9
LunarFang416 <73078183+LunarFang416@users.noreply.github.com>
2021-04-23 19:05:21 -0400

    Create startHi.java

---
+++ JAVA/Warmup-1/startHi.java
@@ -0,0 +1,6 @@
+//Given a string, return true if the string starts with "hi" and false otherwise.
+
+public boolean startHi(String str) {
+  if(str.length() < 2) return false;
+  return str.substring(0,2).equals("hi");
+}
