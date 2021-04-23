502fdadce42ab41a05d8151f234b96a7784f11cd
LunarFang416 <73078183+LunarFang416@users.noreply.github.com>
2021-04-23 19:34:59 -0400

    Create close10.java

---
+++ JAVA/Warmup-1/close10.java
@@ -0,0 +1,8 @@
+//Given 2 int values, return whichever value is nearest to the value 10, or return 0 in 
+//the event of a tie. Note that Math.abs(n) returns the absolute value of a number.
+
+public int close10(int a, int b) {
+  if(Math.abs(10-a) > Math.abs(10-b)) return b;
+  if(Math.abs(10-a) < Math.abs(10-b)) return a;
+  else return 0;
+}
